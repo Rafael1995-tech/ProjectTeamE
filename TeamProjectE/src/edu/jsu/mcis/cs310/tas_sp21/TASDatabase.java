159e40b40822847cb0468dde11a51aaa064254ab
Adam Parker <aparker17@stu.jsu.edu>
2021-03-09 16:02:17 -0600

    "Commiting TASDatabase.java"

--- TeamProjectE/src/edu/jsu/mcis/cs310/tas_sp21/TASDatabase.java
+++ TeamProjectE/src/edu/jsu/mcis/cs310/tas_sp21/TASDatabase.java
@@ -1,14 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.jsu.mcis.cs310.tas_sp21;
+
+import java.sql.*;
 
 /**
  *
- * @author rafaramos
+ * @author adamparker
  */
 public class TASDatabase {
-    
+    Connection conn = null;
+    PreparedStatement pstSelect = null, pstUpdate = null;
+    ResultSet resultset = null;
+    ResultSetMetaData metadata = null;
+    String query;
+    public TASDatabase(){
+        try{
+            String server = ("jdbc:mysql://localhost/TAS.sql");
+            String username = "root";
+            String password = "root";
+        }
+    }
+    public void close(){
+        try{
+            conn.close();
+        }
+        catch(Exception e){};
+    }
+    public Punch getPunch(int punchID){
+        Punch newPunch = new Punch();
+        
+        return newPunch;
+    }
+    public Badge getBadge(int badgeID){
+        Badge newBadge = new Badge();
+        
+        return newBadge;
+    }
+    public Shift getShift(int databaseID){
+        Shift newShift = new Shift();
+        
+        return newShift;
+    }
+    public Shift getShift(Badge userBadge){
+        Shift newShift = new Shift();
+        
+        return newShift;
+    }
 }
