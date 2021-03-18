7df2d7d7b1fce861f2dc597592aba1b6c1bc8c8f
Jay-Batiste <trackhead2k@gmail.com>
2021-03-18 17:21:40 -0400

    Revising Shift

--- TeamProjectE/src/edu/jsu/mcis/cs310/tas_sp21/Shift.java
+++ TeamProjectE/src/edu/jsu/mcis/cs310/tas_sp21/Shift.java
@@ -1,5 +1,7 @@
 package edu.jsu.mcis.cs310.tas_sp21;
+import java.time.LocalTime;
 
+import static java.time.temporal.ChronoUnit.MINUTES;
 import java.sql.Timestamp;
 
 public class Shift {
@@ -10,120 +12,106 @@ public class Shift {
     //Shift table as separate values, use the HOUR() and MINUTE() functions of MySQL.
     //"Shift Start” and “Shift Stop"
     //“Lunch Start” and “Lunch Stop” 
-    int id;
+    private String shift;
+    private String id;
+    private String description;    
+    
+    private int interval;
+    private int gracePeriod;
+    private int dock;
+    private LocalTime lunchStart;
+    private LocalTime lunchStop;
+    private long lunchDuration;
+    private int lunchDeduct;
+    private long shiftDuration;
+    
+    private LocalTime start;
+    private LocalTime stop;
+    
 
-    String description;
-
-    Timestamp start;
-
-    Timestamp stop;
-
-    int interval;
-
-    int gracePeriod;
-
-    int dock;
-
-    Timestamp lunchStart;
-
-    Timestamp lunchStop;
-
-    int lunchDeduct;
-
-    public int getId() {
-        return id;
+    public Shift(String id, String description, LocalTime start, LocalTime stop, int interval, int graceperiod, int dock, int lunchstart, int lunchstop, int lunchdeduct) {
+        
+        int starthour = 7;  // delete later!
+        int startminute = 0; // delete later!
+        
+        this.start = LocalTime.of(starthour, startminute);
+        
+        this.shift = shift;
+        this.id = id;
+        this.description = description;
+        this.start = start;
+        this.stop = stop;
+        this.interval = interval;
+        this.gracePeriod = graceperiod;
+        this.dock = dock;
+        this.lunchStart = lunchStart;
+        this.lunchStop = lunchStop;
+        this.lunchDeduct = lunchDeduct;
+        this.lunchDuration = MINUTES.between(lunchStart, lunchStop);
+        this.shiftDuration = MINUTES.between(start, stop);
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public String getId() {
+        return id;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
-    public Timestamp getStart() {
+
+    public LocalTime getStart() {
         return start;
     }
 
-    public void setStart(Timestamp start) {
-        this.start = start;
-    }
-
-    public Timestamp getStop() {
+    public LocalTime getStop() {
         return stop;
-    }
-
-    public void setStop(Timestamp stop) {
-        this.stop = stop;
     }
 
     public int getInterval() {
         return interval;
     }
 
-    public void setInterval(int interval) {
-        this.interval = interval;
-    }
 
     public int getGracePeriod() {
         return gracePeriod;
     }
 
-    public void setGracePeriod(int gracePeriod) {
-        this.gracePeriod = gracePeriod;
-    }
 
     public int getDock() {
         return dock;
     }
 
-    public void setDock(int dock) {
-        this.dock = dock;
-    }
-
-    public Timestamp getLunchStart() {
+    public LocalTime getLunchstart() {
         return lunchStart;
     }
 
-    public void setLunchStart(Timestamp lunchStart) {
-        this.lunchStart = lunchStart;
-    }
-
-    public Timestamp getLunchStop() {
+    public LocalTime getLunchstop() {
         return lunchStop;
     }
-
-    public void setLunchStop(Timestamp lunchStop) {
-        this.lunchStop = lunchStop;
-    }
-
+ 
     public int getLunchDeduct() {
         return lunchDeduct;
     }
-
-    public void setLunchDeduct(int lunchDeduct) {
-        this.lunchDeduct = lunchDeduct;
+    public long getLunchDuration() {
+        return lunchDuration;
     }
-
-    @Override
-    public String toString() {
-        return description + ": " + DateTimeUtils.convertTimestampToString(start)
-                + " - "
-                + DateTimeUtils.convertTimestampToString(stop)
-                + " ("
-                + DateTimeUtils.timeStampDifference(start, stop)
-                + " minutes); Lunch: "
-                + DateTimeUtils.convertTimestampToString(lunchStart)
-                + " - "
-                + DateTimeUtils.convertTimestampToString(lunchStop)
-                + " ("
-                + DateTimeUtils.timeStampDifference(lunchStart, lunchStop)
-                + " minutes)";
+@Override
+ public String toString() {
+        
+        //REFERENCE: description: start time - stop time, 
+        //EXAMPLE1: "Shift 1: 07:00 - 15:30 (510 minutes); Lunch: 12:00 - 12:30 (30 minutes)"
+        //EXAMPLE2: "Shift Early Lunch: 07:00 - 15:30 (510 minutes); Lunch: 11:30 - 12:00 (30 minutes)"
+        StringBuilder s = new StringBuilder();
+        
+        s.append(description).append(": ");
+        s.append(start).append(" - ").append(stop);
+        s.append(" (").append(shiftDuration).append(" minutes);");
+        s.append(" Lunch: ").append(lunchStart).append(" - ").append(lunchStop);
+        s.append(" (").append(lunchDuration).append(" minutes)");
+        
+        return s.toString();
     }
-
 }
+
